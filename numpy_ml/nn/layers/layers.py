420821f44646dcb678a0e78deeb73a8a05c95e3a
ley <ley@opera.com>
2025-04-17 15:05:20 +0800

    add flatten, batchnorm2d, batchnorm1d layers

--- numpy_ml/nn/layers/layers.py
+++ numpy_ml/nn/layers/layers.py
@@ -13,12 +13,11 @@ class LayerBase(ABC):
 
     def __init__(self):
         """An abstract base class inherited by all neural network layers"""
-        self.trainable = True
+        super().__init__()
 
+        self.trainable = True
         self.parameters: Dict[str, Optional[Parameter]] = {}
         self.derived_variables: Dict[str, Optional[Parameter]] = {"x": None, "out": None}
-
-        super().__init__()
 
     def __call__(self, *args, **kwargs):
         return self.forward(*args, **kwargs)
@@ -114,7 +113,6 @@ class Linear(LayerBase):
         b = Parameter(b)
 
         self.parameters: Dict[str, Optional[Parameter]] = {"W": W, "b": b}
-        self.derived_variables: Dict[str, Optional[Parameter]] = {"out": None}
 
     @property
     def hyperparameters(self):
@@ -167,3 +165,304 @@ class Linear(LayerBase):
         db = dldy.sum(axis=0, keepdims=True)
 
         return dx, dw, db
+
+
+class Flatten(LayerBase):
+    r"""
+        https://pytorch.org/docs/stable/generated/torch.nn.Flatten.html
+
+        Flattens a contiguous range of dimensions into a single dimension.
+
+        Example:
+        - input.shape: (B, C, H, W)
+        - output.shape: (B, C*H*W)  # if start_dim=1, end_dim=-1
+        - output.shape: (B*C*H*W,) # if start_dim=0, end_dim=-1
+    """
+
+    def __init__(self, start_dim=1, end_dim=-1):
+        super().__init__()
+        self.start_dim = start_dim
+        self.end_dim = end_dim
+        self.parameters = {}
+        self.derived_variables = {"input_shape": None, "x": None, "out": None}
+
+        self._init_params()
+
+    def _init_params(self):
+        self.parameters = {}
+
+    @property
+    def hyperparameters(self):
+        return {
+            "layer": self.__class__.__name__,
+            "start_dim": self.start_dim,
+            "end_dim": self.end_dim,
+        }
+
+    def forward(self, X: np.ndarray, retain_derived=True) -> np.ndarray:
+        self._check_input(X)
+        input_shape = X.shape
+
+        start = self.start_dim if self.start_dim >= 0 else X.ndim + self.start_dim
+        end = self.end_dim if self.end_dim >= 0 else X.ndim + self.end_dim
+
+        flattened_dim = int(np.prod(X.shape[start:end + 1]))
+        new_shape = X.shape[:start] + (flattened_dim,) + X.shape[end + 1:]
+
+        out = X.reshape(new_shape)
+
+        if retain_derived:
+            self.derived_variables["x"] = Parameter(X)
+            self.derived_variables["out"] = Parameter(out)
+            self.derived_variables["input_shape"] = input_shape
+
+        return out
+
+    def backward(self, dldy: np.ndarray, retain_grads=True):
+        input_shape = self.derived_variables["input_shape"]
+        dx = dldy.reshape(input_shape)
+        if retain_grads:
+            self.derived_variables["x"].grad = dx
+            self.derived_variables["out"].grad = dldy
+
+        return dx
+
+
+class BatchNorm2D(LayerBase):
+    r"""
+        https://pytorch.org/docs/stable/generated/torch.nn.BatchNorm2d.html
+
+        Applies Batch Normalization over a 4D input (a mini-batch of 2D inputs with additional channel dimension)
+        Expected input shape: (N, C, H, W)
+    """
+
+    def __init__(self, num_features, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True):
+        super().__init__()
+        self.num_features = num_features
+        self.eps = eps
+        self.momentum = momentum
+        self.affine = affine
+        self.track_running_stats = track_running_stats
+        self.parameters = {"gamma": None, "beta": None}
+        self.derived_variables = {"x": None, "out": None, "x_centered": None, "std_inv": None, "x_norm": None}
+
+        self._init_params()
+
+    def _init_params(self):
+        self.parameters: Dict[str, Optional[Parameter]] = {
+            "gamma": Parameter(np.ones((1, self.num_features, 1, 1))) if self.affine else None,
+            "beta": Parameter(np.zeros((1, self.num_features, 1, 1))) if self.affine else None,
+        }
+
+        self.running_mean = np.zeros((1, self.num_features, 1, 1)) if self.track_running_stats else None
+        self.running_var = np.ones((1, self.num_features, 1, 1)) if self.track_running_stats else None
+
+        self.derived_variables: Dict[str, Optional[Parameter]] = {
+            "x_centered": None,
+            "std_inv": None,
+            "x_norm": None,
+        }
+
+    @property
+    def hyperparameters(self):
+        return {
+            "layer": self.__class__.__name__,
+            "num_features": self.num_features,
+            "eps": self.eps,
+            "momentum": self.momentum,
+            "affine": self.affine,
+            "track_running_stats": self.track_running_stats,
+        }
+
+    def _check_input(self, x: np.ndarray):
+        if x.ndim != 4:
+            raise ValueError(f"expected 4D input (got {x.ndim}D input)")
+
+    def forward(self, X: np.ndarray, retain_derived=True):
+        self._check_input(X)
+
+        if self.trainable:
+            mean = X.mean(axis=(0, 2, 3), keepdims=True)
+            var = X.var(axis=(0, 2, 3), keepdims=True)
+            if self.track_running_stats:
+                self.running_mean = (1 - self.momentum) * self.running_mean + self.momentum * mean
+                self.running_var = (1 - self.momentum) * self.running_var + self.momentum * var
+        else:
+            if self.track_running_stats:
+                mean = self.running_mean
+                var = self.running_var
+            else:
+                mean = X.mean(axis=(0, 2, 3), keepdims=True)
+                var = X.var(axis=(0, 2, 3), keepdims=True)
+
+        X_centered = X - mean
+        std_inv = 1.0 / np.sqrt(var + self.eps)
+        X_norm = X_centered * std_inv
+
+        if self.affine:
+            out = self.parameters["gamma"].data * X_norm + self.parameters["beta"].data
+        else:
+            out = X_norm
+
+        if retain_derived:
+            self.derived_variables["x"] = Parameter(X)
+            self.derived_variables["out"] = Parameter(out)
+            self.derived_variables["x_centered"] = Parameter(X_centered)
+            self.derived_variables["std_inv"] = Parameter(std_inv)
+            self.derived_variables["x_norm"] = Parameter(X_norm)
+
+        return out
+
+    def backward(self, dldy: np.ndarray, retain_grads=True):
+        X_centered = self.derived_variables["x_centered"].data
+        std_inv = self.derived_variables["std_inv"].data
+        X_norm = self.derived_variables["x_norm"].data
+        N, C, H, W = dldy.shape
+        m = N * H * W
+
+        dX_norm = dldy * self.parameters["gamma"].data if self.affine else dldy
+
+        dvar = np.sum(dX_norm * X_centered, axis=(0, 2, 3), keepdims=True) * -0.5 * (std_inv**3)
+        dmean = np.sum(dX_norm * -std_inv, axis=(0, 2, 3), keepdims=True) + dvar * \
+            np.mean(-2. * X_centered, axis=(0, 2, 3), keepdims=True)
+        dX = dX_norm * std_inv + dvar * 2 * X_centered / m + dmean / m
+
+        if retain_grads:
+            self.derived_variables["x"].grad = dX
+            self.derived_variables["out"].grad = dldy
+
+            if self.affine:
+                self.parameters["gamma"].grad += np.sum(dldy * X_norm, axis=(0, 2, 3), keepdims=True)
+                self.parameters["beta"].grad += np.sum(dldy, axis=(0, 2, 3), keepdims=True)
+
+        return dX
+
+
+class BatchNorm1D(LayerBase):
+    r"""
+    Applies Batch Normalization over a 2D or 3D input.
+    Expected input shape: (N, C) or (N, C, L)
+    """
+
+    def __init__(self, num_features, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True):
+        super().__init__()
+        self.num_features = num_features
+        self.eps = eps
+        self.momentum = momentum
+        self.affine = affine
+        self.track_running_stats = track_running_stats
+
+        self._init_params()
+
+    def _init_params(self):
+        shape = (self.num_features,)
+        if self.affine:
+            self.parameters = {
+                "gamma": Parameter(np.ones(shape, dtype=np.float32)),
+                "beta": Parameter(np.zeros(shape, dtype=np.float32)),
+            }
+        else:
+            self.parameters = {"gamma": None, "beta": None}
+
+        if self.track_running_stats:
+            self.running_mean = np.zeros(shape, dtype=np.float32)
+            self.running_var = np.ones(shape, dtype=np.float32)
+        else:
+            self.running_mean = None
+            self.running_var = None
+
+        self.derived_variables = {
+            "x": None,
+            "out": None,
+            "x_centered": None,
+            "std_inv": None,
+            "x_norm": None,
+        }
+
+    @property
+    def hyperparameters(self):
+        return {
+            "layer": self.__class__.__name__,
+            "num_features": self.num_features,
+            "eps": self.eps,
+            "momentum": self.momentum,
+            "affine": self.affine,
+            "track_running_stats": self.track_running_stats,
+        }
+
+    def _check_input(self, x: np.ndarray):
+        if x.ndim not in [2, 3]:
+            raise ValueError(f"Expected 2D or 3D input (got {x.ndim}D input)")
+
+    def forward(self, X: np.ndarray, retain_derived=True):
+        self._check_input(X)
+
+        is_2d = X.ndim == 2
+        if is_2d:
+            X = X[:, :, None]  # (N, C) -> (N, C, 1)
+
+        mean = X.mean(axis=(0, 2)).astype(np.float32)   # (C, )
+        var = X.var(axis=(0, 2)).astype(np.float32)     # (C, )
+
+        if self.trainable:
+            if self.track_running_stats:
+                self.running_mean = (1 - self.momentum) * self.running_mean + self.momentum * mean
+                self.running_var = (1 - self.momentum) * self.running_var + self.momentum * var
+        else:
+            mean = self.running_mean if self.track_running_stats else mean
+            var = self.running_var if self.track_running_stats else var
+
+        mean = mean.reshape(1, -1, 1)
+        std = np.sqrt(var + self.eps).reshape(1, -1, 1)
+
+        x_centered = X - mean
+        x_norm = x_centered / std
+
+        if self.affine:
+            gamma = self.parameters["gamma"].data.reshape(1, -1, 1)
+            beta = self.parameters["beta"].data.reshape(1, -1, 1)
+            out = gamma * x_norm + beta
+        else:
+            out = x_norm
+
+        if retain_derived:
+            self.derived_variables["x"] = Parameter(X)
+            self.derived_variables["out"] = Parameter(out)
+            self.derived_variables["x_centered"] = x_centered
+            self.derived_variables["std_inv"] = 1.0 / std
+            self.derived_variables["x_norm"] = x_norm
+
+        return out.squeeze(-1) if is_2d else out
+
+    def backward(self, dldy: np.ndarray, retain_grads=True):
+        is_2d = dldy.ndim == 2
+        if is_2d:
+            dldy = dldy[:, :, None]
+
+        X_centered = self.derived_variables["x_centered"]
+        std_inv = self.derived_variables["std_inv"]
+        x_norm = self.derived_variables["x_norm"]
+        N, C, L = dldy.shape
+        m = N * L
+
+        if self.affine:
+            gamma = self.parameters["gamma"].data.reshape(1, C, 1)
+            dX_norm = dldy * gamma
+        else:
+            dX_norm = dldy
+
+        dvar = np.sum(dX_norm * X_centered, axis=(0, 2), keepdims=True) * -0.5 * (std_inv**3)
+        dmean = np.sum(dX_norm * -std_inv, axis=(0, 2), keepdims=True) + \
+            dvar * np.mean(-2. * X_centered, axis=(0, 2), keepdims=True)
+
+        dX = dX_norm * std_inv + dvar * 2 * X_centered / m + dmean / m
+
+        if retain_grads:
+            if self.affine:
+                self.parameters["gamma"].grad = np.sum(dldy * x_norm, axis=(0, 2)).reshape(-1)
+                self.parameters["beta"].grad = np.sum(dldy, axis=(0, 2)).reshape(-1)
+
+            self.derived_variables["x"].grad = dX
+            self.derived_variables["out"].grad = dldy
+
+        return dX.squeeze(-1) if is_2d else dX
--- numpy_ml/nn/testing/test_layers.py
+++ numpy_ml/nn/testing/test_layers.py
@@ -3,7 +3,8 @@ import torch
 import torch.nn as nn
 import pytest
 
-from nn.layers import Linear  # 根据你的实际导入路径调整
+from copy import deepcopy
+from nn.layers import Linear, Flatten, BatchNorm2D, BatchNorm1D
 
 
 @pytest.mark.parametrize("batch_size, in_feat, out_feat", [
@@ -12,18 +13,14 @@ from nn.layers import Linear  # 根据你的实际导入路径调整
     (1, 6, 4)
 ])
 def test_linear(batch_size, in_feat, out_feat):
-    # 设置种子以保证一致性
     np.random.seed(42)
     torch.manual_seed(42)
 
-    # 输入张量
     X_np = np.random.randn(batch_size, in_feat).astype(np.float32)
-    X_torch = torch.tensor(X_np, requires_grad=True)
+    X_torch = torch.tensor(deepcopy(X_np), requires_grad=True)
 
-    # 自定义 Linear
     my_fc = Linear(in_feat, out_feat)
 
-    # PyTorch Linear
     pt_fc = nn.Linear(in_feat, out_feat, bias=True)
     pt_fc.weight.data = torch.tensor(my_fc.parameters["W"].data, dtype=torch.float32)
     pt_fc.bias.data = torch.tensor(my_fc.parameters["b"].data.squeeze(), dtype=torch.float32)
@@ -31,35 +28,188 @@ def test_linear(batch_size, in_feat, out_feat):
     np.testing.assert_allclose(my_fc.parameters["b"].data.squeeze(),
                                pt_fc.bias.data.detach().numpy(),  rtol=1e-5, atol=1e-6)
 
-    # 前向传播
-    out_my = my_fc.forward(X_np)
+    out_my = my_fc(X_np)
     out_pt = pt_fc(X_torch)
 
-    # 检查前向传播输出是否一致
     np.testing.assert_allclose(out_my, out_pt.detach().numpy(), rtol=1e-5, atol=1e-6)
 
-    # 构造 dL/dy 模拟上游梯度
     dLdy_np = np.random.randn(*out_my.shape).astype(np.float32)
-    dLdy_pt = torch.tensor(dLdy_np)
+    dLdy_pt = torch.tensor(deepcopy(dLdy_np))
 
-    # 后向传播
     dX_my = my_fc.backward(dLdy_np)
     out_pt.backward(dLdy_pt)
 
-    # 取出 PyTorch 的梯度
     dX_pt = X_torch.grad.detach().numpy()
     dW_pt = pt_fc.weight.grad.detach().numpy()
     dB_pt = pt_fc.bias.grad.detach().numpy()
 
-    # 自定义模型的梯度
     dW_my = my_fc.parameters["W"].grad
     dB_my = my_fc.parameters["b"].grad.squeeze()
 
-    # 检查梯度是否一致
     np.testing.assert_allclose(dX_my, dX_pt, rtol=1e-5, atol=1e-6)
     np.testing.assert_allclose(dW_my, dW_pt, rtol=1e-5, atol=1e-6)
     np.testing.assert_allclose(dB_my, dB_pt, rtol=1e-5, atol=1e-6)
 
 
+class TestFlatten:
+    @pytest.mark.parametrize("start_dim, end_dim, shape", [
+        (1, -1, (2, 3, 4)),   # flatten [3, 4] to [12]
+        (0, -1, (2, 3, 4)),   # flatten all
+        (1, 2, (2, 3, 4)),    # flatten [3, 4] to [12]
+        (0, 1, (3, 4)),       # flatten first two dims
+    ])
+    def test_forward_and_backward(self, shape, start_dim, end_dim):
+        x_np = np.random.randn(*shape).astype(np.float32)
+        x_torch = torch.tensor(deepcopy(x_np), requires_grad=True)
+
+        my_layer = Flatten(start_dim=start_dim, end_dim=end_dim)
+        out_np = my_layer.forward(x_np)
+
+        torch_layer = torch.nn.Flatten(start_dim=start_dim, end_dim=end_dim)
+        out_torch = torch_layer(x_torch)
+
+        np.testing.assert_allclose(out_np, out_torch.detach().numpy(), atol=1e-6, rtol=1e-5)
+
+        grad_out = np.random.randn(*out_np.shape).astype(np.float32)
+        grad_out_torch = torch.tensor(deepcopy(grad_out))
+
+        grad_in_np = my_layer.backward(grad_out)
+        out_torch.backward(grad_out_torch)
+        grad_in_torch = x_torch.grad.detach().numpy()
+
+        np.testing.assert_allclose(grad_in_np, grad_in_torch, atol=1e-6, rtol=1e-5)
+
+
+class TestBatchNorm2D:
+    @pytest.mark.parametrize("affine, track_running_stats, training", [
+        (True, True, True),
+        (True, True, False),
+        (True, False, True),
+        (True, False, False),
+        (False, True, True),
+        (False, True, False),
+        (False, False, True),
+        (False, False, False),
+    ])
+    def test_batchnorm2d_vs_pytorch(self, affine, track_running_stats, training):
+        np.random.seed(42)
+        torch.manual_seed(42)
+
+        N, C, H, W = 4, 3, 5, 5
+        x_np = np.random.randn(N, C, H, W).astype(np.float32)
+        x_torch = torch.tensor(x_np, dtype=torch.float32, requires_grad=True)
+
+        # PyTorch BN
+        bn_torch = nn.BatchNorm2d(C, eps=1e-5, momentum=0.1, affine=affine, track_running_stats=track_running_stats)
+        bn_torch.train(training)
+        out_torch = bn_torch(x_torch)
+
+        # Our BN
+        bn_np = BatchNorm2D(C, eps=1e-5, momentum=0.1, affine=affine, track_running_stats=track_running_stats)
+        bn_np.trainable = training
+        out_np = bn_np.forward(x_np.copy())
+
+        # Compare outputs
+        np.testing.assert_allclose(out_np, out_torch.detach().numpy(), rtol=1e-4, atol=1e-4)
+
+        # Compare running stats
+        if track_running_stats:
+            np.testing.assert_allclose(bn_np.running_mean, bn_torch.running_mean.detach(
+            ).numpy().reshape(1, C, 1, 1), rtol=1e-3, atol=1e-3)
+            np.testing.assert_allclose(bn_np.running_var, bn_torch.running_var.detach(
+            ).numpy().reshape(1, C, 1, 1), rtol=1e-3, atol=1e-3)
+
+        # Compare affine params if used
+        if affine:
+            # Simulate param sync
+            gamma = torch.nn.Parameter(torch.randn(1, C, 1, 1))
+            beta = torch.nn.Parameter(torch.randn(1, C, 1, 1))
+            bn_torch.weight.data = gamma.view(C)
+            bn_torch.bias.data = beta.view(C)
+            bn_np.parameters["gamma"].data = gamma.detach().numpy()
+            bn_np.parameters["beta"].data = beta.detach().numpy()
+
+            out_torch = bn_torch(x_torch)
+            out_np = bn_np.forward(x_np.copy())
+
+            np.testing.assert_allclose(out_np, out_torch.detach().numpy(), rtol=1e-4, atol=1e-4)
+
+            # Backward check
+            dout_np = np.random.randn(*out_np.shape).astype(np.float32)
+            out_torch.backward(torch.tensor(dout_np))
+            bn_np.backward(dout_np)
+
+            np.testing.assert_allclose(bn_np.parameters["gamma"].grad,
+                                       bn_torch.weight.grad.view(1, C, 1, 1).numpy(), rtol=1e-4, atol=1e-4)
+            np.testing.assert_allclose(bn_np.parameters["beta"].grad,
+                                       bn_torch.bias.grad.view(1, C, 1, 1).numpy(), rtol=1e-4, atol=1e-4)
+
+
+class TestBatchNorm1D:
+    @pytest.mark.parametrize("affine, track_running_stats, training", [
+        (True, True, True),
+        (True, True, False),
+        (True, False, True),
+        (True, False, False),
+        (False, True, True),
+        (False, True, False),
+        (False, False, True),
+        (False, False, False),
+    ])
+    @pytest.mark.parametrize("shape", [(8, 4), (8, 4, 10)])  # (N, C) and (N, C, L)
+    def test_batchnorm1d_vs_pytorch(self, affine, track_running_stats, training, shape):
+        np.random.seed(0)
+        torch.manual_seed(0)
+
+        C = shape[1]
+
+        # 初始化 PyTorch BN
+        bn_torch = nn.BatchNorm1d(C, eps=1e-5, momentum=0.1,
+                                  affine=affine, track_running_stats=track_running_stats)
+        bn_torch.train(training)
+
+        # 初始化自定义 BN
+        bn_np = BatchNorm1D(C, eps=1e-5, momentum=0.1,
+                            affine=affine, track_running_stats=track_running_stats)
+        bn_np.trainable = training
+
+        # 设置相同的 affine 参数（如果需要）
+        if affine:
+            gamma = torch.randn(1, C, *([1] * (len(shape) - 2)))
+            beta = torch.randn(1, C, *([1] * (len(shape) - 2)))
+            bn_torch.weight.data = gamma.view(C)
+            bn_torch.bias.data = beta.view(C)
+            bn_np.parameters["gamma"].data = gamma.numpy()
+            bn_np.parameters["beta"].data = beta.numpy()
+
+        # forward
+        for _ in range(10):
+            x_np = np.random.randn(*shape).astype(np.float32)
+            x_torch = torch.tensor(x_np, dtype=torch.float32, requires_grad=True)
+
+            out_torch = bn_torch(x_torch)
+            out_np = bn_np.forward(x_np.copy())
+
+        np.testing.assert_allclose(out_np, out_torch.detach().numpy(), rtol=1e-4, atol=1e-4)
+
+        # running stats 检查
+        if track_running_stats:
+            np.testing.assert_allclose(bn_np.running_mean, bn_torch.running_mean.detach().numpy(), rtol=5e-2, atol=5e-2)
+            np.testing.assert_allclose(bn_np.running_var, bn_torch.running_var.detach().numpy(), rtol=5e-2, atol=5e-2)
+
+        # backward（仅在 affine 时检查 gamma/beta 的梯度）
+        if affine and training:
+            dout_np = np.random.randn(*shape).astype(np.float32)
+            dout_torch = torch.tensor(dout_np, dtype=torch.float32)
+
+            out_torch.backward(dout_torch)
+            bn_np.backward(dout_np)
+
+            np.testing.assert_allclose(bn_np.parameters["gamma"].grad,
+                                       bn_torch.weight.grad.detach().numpy(), rtol=1e-4, atol=1e-4)
+            np.testing.assert_allclose(bn_np.parameters["beta"].grad,
+                                       bn_torch.bias.grad.detach().numpy(), rtol=1e-4, atol=1e-4)
+
+
 if __name__ == "__main__":
-    test_linear(32, 10, 2)
+    pytest.main(["-v", __file__])
